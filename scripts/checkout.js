72d6323d6187088adc1e011b806a91cd5c1419c6
Abhiraj028 <abhirajgautam028@gmail.com>
2025-02-28 11:34:32 +0530

    learn promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,45 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
-loadProducts(()=>{
+// loadProducts(()=>{
+//     loadCart(() =>{
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//     });
+// });
+
+
+// new Promise((resolve) => {
+//     loadProducts(() => {
+//         resolve();
+//     })
+// }).then(() => {
+//     return new Promise((resolve) =>{
+//         loadCart(() =>{
+//             resolve();
+//         })
+//     })
+// }).then(() =>{
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// })
+ 
+Promise.all([
+    new Promise((resolve) =>{
+        loadProducts(()=>{
+            resolve("Inside resolve vigga");
+        })
+    }),
+
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve("Inside resolve pt2 blicka");
+        })
+    })
+]).then((values)=>{
+    console.log(values);
     renderOrderSummary();
     renderPaymentSummary();
 })
- 
